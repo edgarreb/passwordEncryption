b40e678464ccf27c244971d401e43d9bbd3c3e42
Edgar Martinez <edgarmarreb@gmail.com>
2020-01-17 16:04:59 -0800

    Random offset ecryption/decryption working

--- encryption.cpp
+++ encryption.cpp
@@ -10,21 +10,22 @@
 using namespace std;
 
 const int offset = 13;
+const int additionalChars = 3;
 
 void caesarCipher (string file1, string file2) {
 
-	// open a file and read from it 
+	// open a file and read from it
 	ifstream inputFile;
 	inputFile.open(file1);
 
 	if(inputFile.bad() == true) {
-	
+
 		cout << "ERROR: unable to read file.." << endl;
 		inputFile.close();
 	}
 	else {
-		
-		// open file to write to 
+
+		// open file to write to
 		ofstream outputFile;
 		outputFile.open(file2);
 
@@ -33,47 +34,113 @@ void caesarCipher (string file1, string file2) {
 		string str;
 
 		while(inputFile.eof() != true) {
-			
+
 			getline(inputFile, str);
-		
-			// convert ASCII to int 
+
+			// convert ASCII to int
 			for(int i = 0; i < str.length(); i++) {
-						
-				int position = (int) str[i]; 
+
+				int position = (int) str[i];
 				int encodedPosition = position + offset;
-				
+
 				if(encodedPosition > 126) {
-					
+
 					int newOffset = encodedPosition - 126;
-					encodedPosition = 32 + newOffset; 
+					encodedPosition = 32 + newOffset;
 					outputFile << (char) encodedPosition;
-				}	
+				}
 				else {
-					
+
 					outputFile << (char) encodedPosition;
 				}
 			}
 
 			outputFile << endl;
-			
+
 		}
 
 		inputFile.close();
-		outputFile.close(); 
+		outputFile.close();
 	}
 
 }
 
+void randomOffset(string originalFile, string encryptedFile, string keyFile) {
+
+	// open a file and read from it
+	ifstream inputFile;
+	inputFile.open(originalFile);
+
+	if(inputFile.bad() == true) {
+
+		cout << "ERROR: unable to read file.." << endl;
+		inputFile.close();
+	}
+	else {
+
+		// open file to write to
+		ofstream outputFile;
+		outputFile.open(encryptedFile);
+
+		ofstream outputFile2;
+		outputFile2.open(keyFile);
+
+		// reset the state flags of inputFile so that we can enter the loop
+		inputFile.clear();
+		string tempString;
+
+		int randomOffset;
+		int position;
+
+		while(inputFile.eof() != true) {
+
+			getline(inputFile, tempString);
+
+			// convert ASCII to int
+			for(int i = 0; i < tempString.length(); i++) {
+
+				randomOffset = rand() % 100;
+				position = (int) tempString[i];
+				int encodedPosition = position + randomOffset;
+
+				// write the random offset to the keyFile
+				outputFile2 << randomOffset << endl;
+
+				while(encodedPosition > 126) {
+
+					int newOffset = encodedPosition - 126;
+					encodedPosition = 32 + newOffset;
+					//outputFile << (char) encodedPosition;
+				}
+
+				outputFile << (char) encodedPosition;
+
+				}
+
+				outputFile << endl;
+			}
+
+			outputFile.close();
+			outputFile2.close();
+
+		}
+
+		inputFile.close();
+
+}
+
 int main() {
-	
-	string file1, file2; 
 
-	cout << "Input file: ";
+	string file1, file2, file3;
+
+	cout << "Original File: ";
 		cin >> file1;
-	cout << "Name of encrypted file: ";
+	cout << "Encrypted File: ";
 		cin >> file2;
+	cout << "Key File: ";
+		cin >> file3;
 
-	caesarCipher(file1, file2);	
+	randomOffset(file1, file2, file3);
 
 	return 0;
 }
--- unlock.cpp
+++ unlock.cpp
@@ -9,10 +9,10 @@
 
 using namespace std;
 
-// offset used to encrypt/decrypt
-const int offset = 13;
-
 void decrypt(string file1, string file2) {
+
+	// offset used to encrypt/decrypt
+	const int offset = 13;
 
 	ifstream inputFile;
 	inputFile.open(file1);
@@ -21,26 +21,26 @@ void decrypt(string file1, string file2) {
 	outputFile.open(file2);
 
 	if(inputFile.bad() == true) {
-		
+
 		cout << "ERROR: Unable to read file.." << endl;
 	}
 	else {
-	
+
 		string line;
-		
+
 		while(inputFile.eof() != true) {
-			
+
 			getline(inputFile, line);
-	
+
 			for(int i = 0; i < line.length(); i++) {
-				
+
 				char tempChar = (char) line[i];
 				int temp = (int) tempChar;
-				
+
 				int decodedPosition = temp - offset;
 
 				if(decodedPosition < 32) {
-					
+
 					int newOffset = 32 - decodedPosition;
 					int newPosition = 126 - newOffset;
 					outputFile << (char) newPosition;
@@ -50,9 +50,9 @@ void decrypt(string file1, string file2) {
 					outputFile << (char) decodedPosition;
 				}
 			}
-			
-			outputFile << endl; 
-		}	
+
+			outputFile << endl;
+		}
 
 	}
 
@@ -61,15 +61,89 @@ void decrypt(string file1, string file2) {
 
 }
 
+void randomDecrypt(string encryptedFile, string keyFile, string unencryptedFile) {
+
+	ifstream inputFile;
+	inputFile.open(encryptedFile);
+
+	ifstream inputFile2;
+	inputFile2.open(keyFile);
+
+	ofstream outputFile;
+	outputFile.open(unencryptedFile);
+
+	if( (inputFile.bad() == true) || (inputFile2.bad() == true) ) {
+
+		cout << "ERROR: Unable to read file.." << endl;
+	}
+	else {
+
+		string line;
+		string offsetLine;
+
+		char tempChar;
+		int randomOffset;
+		int encodedPosition;
+		int decodedPosition;
+
+		while(inputFile.eof() != true) {
+
+			getline(inputFile, line);
+
+			for(int i = 0; i < line.length(); i++) {
+
+				getline(inputFile2, offsetLine);
+
+				tempChar = (char) line[i];
+				encodedPosition = (int) tempChar;
+				randomOffset = stoi(offsetLine);
+				cout << randomOffset << endl;
+
+				decodedPosition = encodedPosition - randomOffset;
+
+				while(decodedPosition < 32) {
+
+					if(decodedPosition >= 0) {
+
+						randomOffset = 32 - decodedPosition;
+						decodedPosition = 126 - randomOffset;
+					}
+					else {
+						randomOffset = abs(decodedPosition) + 32;
+						decodedPosition = 126 - randomOffset;
+					}
+					/*
+					randomOffset = 32 - decodedPosition;
+					decodedPosition = 126 - Offset;
+					//outputFile << (char) newPosition; */
+				}
+
+				outputFile << (char) decodedPosition;
+			}
+
+			outputFile << endl;
+		}
+
+	}
+
+	inputFile.close();
+	inputFile2.close();
+	outputFile.close();
+
+}
+
 int main() {
 
-	string file1, file2;
-	cout << "Input File: "; 
-		cin >> file1;
-	cout <<	"Output File: ";
-		cin >> file2;
+	string file1, file2, file3;
 
-	decrypt(file1, file2);
+	cout << "Encrypted File: ";
+		cin >> file1;
+	cout <<	"Key File: ";
+		cin >> file2;
+	cout << "Unencrypted File: ";
+		cin >> file3;
+
+	randomDecrypt(file1, file2, file3);
 
 	return 0;
 }
